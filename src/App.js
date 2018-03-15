719e6029f00891ac6000e1cf42f256b2b42ca9e8
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2018-03-15 12:23:53 +1100

    Authenticate with Spotify and get access token

--- src/App.js
+++ src/App.js
@@ -2,7 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getCallbackURL = () => {
+  const API_ENDPOINT = `https://accounts.spotify.com`;
+  const REQUEST_SCOPES = ["playlist-read-private", "playlist-read-collaborative", "playlist-modify-public", "user-read-recently-played", "playlist-modify-private", "ugc-image-upload", "user-follow-modify", "user-follow-read", "user-library-read", "user-library-modify", "user-read-private", "user-read-email", "user-top-read", "user-read-playback-state"];
+  const RESPONSE_TYPE = `token`;
+
+  let result = `${API_ENDPOINT}/authorize?`;
+  result += `client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
+  result += `&scope=${REQUEST_SCOPES.join('%20')}`;
+  result += `&response_type=${RESPONSE_TYPE}`;
+  result += `&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}`;
+
+  return result;
+};
+
+const parseAccessToken = () => {
+  const ACCESS_TOKEN_KEY = "access_token";
+  let accessToken = null;
+
+  let params = window.location.hash.substring(1).split("&");
+  params.forEach(keyVal => {
+    let key = keyVal.split("=")[0];
+    let value = keyVal.split("=")[1];
+    if (key === ACCESS_TOKEN_KEY) {
+      accessToken = value
+    }
+  });
+  return accessToken;
+};
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const pathname = window.location.pathname;
+    if (pathname === '/callback') {
+      let accessToken = parseAccessToken();
+      console.log(`Access Token is : ${accessToken}`);
+    } else {
+      window.location.href = getCallbackURL();
+    }
+  }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div className="App">
