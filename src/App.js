beaeea0ed7625b4e45a09e69a17f195d59321642
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2018-03-18 11:43:29 +1100

    (Hack-ily) Fetch a playlist and show its songs.
    This is incredibly hackily done, just to get the proof of concept
    working. It fetches a users playlists, gets the first one, show's the
    songs in it and renders the song name, image and preview track.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 const getCallbackURL = () => {
-  const API_ENDPOINT = `https://accounts.spotify.com`;
-  const REQUEST_SCOPES = ["playlist-read-private", "playlist-read-collaborative", "playlist-modify-public", "user-read-recently-played", "playlist-modify-private", "ugc-image-upload", "user-follow-modify", "user-follow-read", "user-library-read", "user-library-modify", "user-read-private", "user-read-email", "user-top-read", "user-read-playback-state"];
+  const AUTH_ENDPOINT = `https://accounts.spotify.com`;
+  const REQUEST_SCOPES =  ["streaming", "user-read-birthdate", "user-read-email", "user-read-private",
+  "playlist-read-private", "playlist-read-collaborative", "playlist-modify-public", "user-read-recently-played",
+  "playlist-modify-private", "ugc-image-upload", "user-follow-modify", "user-follow-read",
+  "user-library-read", "user-library-modify", "user-top-read", "user-read-playback-state"];
   const RESPONSE_TYPE = `token`;
 
-  let result = `${API_ENDPOINT}/authorize?`;
+  let result = `${AUTH_ENDPOINT}/authorize?`;
   result += `client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
   result += `&scope=${REQUEST_SCOPES.join('%20')}`;
   result += `&response_type=${RESPONSE_TYPE}`;
@@ -31,32 +34,118 @@ const parseAccessToken = () => {
   return accessToken;
 };
 
+const parseJsonResponse = (response) => {
+  let json = response.json();
+
+  if (response.ok) {
+    return json;
+  } else {
+    // Reject the promise if the response is a 500
+    return json.then(err => {throw err;});
+  }
+};
+
+const fetchAllPlaylists = (accessToken) => {
+  const API_ENDPOINT = `https://api.spotify.com/v1`;
+  let url = `${API_ENDPOINT}/me/playlists`;
+  const GET_HEADER = {
+    method: 'GET',
+    mode: 'cors',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    }
+  };
+
+  return fetch(url, GET_HEADER).then(response => parseJsonResponse(response));
+};
+
+const fetchPlaylistDetails = (playlist, accessToken) => {
+  let url = playlist.href;
+  const GET_HEADER = {
+    method: 'GET',
+    mode: 'cors',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    }
+  };
+
+  return fetch(url, GET_HEADER).then(response => parseJsonResponse(response));
+};
+
+const SongImage = ({images, name}) => {
+  let filteredImages = images.filter(img => img.height < 100);
+  if (filteredImages.length > 0) {
+    let image = filteredImages[0];
+    return (
+      <img src={image.url} alt={name} width={image.width} height={image.height} />
+    );
+  } else {
+    return null;
+  }
+};
+
+const SongComponent = ({is_local, track}) => {
+  if (is_local) {
+    return null;
+  } else {
+    return (
+      <div>
+        <SongImage {...track.album}/>
+        <audio controls>
+          <source type="audio/mpeg" src={track.preview_url}/>
+          Audio tag not supported
+        </audio>
+        <p> {track.name} </p>
+        <hr />
+      </div>
+    );
+  }
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    let accessToken = null;
     const pathname = window.location.pathname;
     if (pathname === '/callback') {
-      let accessToken = parseAccessToken();
-      console.log(`Access Token is : ${accessToken}`);
+      accessToken = parseAccessToken();
     } else {
       window.location.href = getCallbackURL();
     }
+
+    this.state = {
+      accessToken: accessToken,
+      songs: []
+    };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    if (this.state.accessToken !== null) {
+      let playlists = await fetchAllPlaylists(this.state.accessToken);
+      let the_playlist = await fetchPlaylistDetails(playlists.items[0], this.state.accessToken);
+      let songs = the_playlist.tracks.items;
+      this.setState({songs});
+    }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Spotify thing.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          Your temporary access token: {this.state.accessToken}
         </p>
+        <div>
+          Songs:
+          {this.state.songs.map((song, index) => <SongComponent {...song} key={`song-${index}`} />)}
+        </div>
       </div>
     );
   }
