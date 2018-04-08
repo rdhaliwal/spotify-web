094d43e05b1ef8e9133ac1bfbb18b7c6c7f9486e
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2018-04-09 09:37:50 +1000

    Fix bug where it wouldn't play song after updating

--- src/components/CurrentTrack.js
+++ src/components/CurrentTrack.js
@@ -13,22 +13,53 @@ const SongImage = ({images, name}) => {
   );
 };
 
-
-export const CurrentTrack = ({track}) => {
-  if (track == null) { return null; }
-
+const TrackDetails = ({track}) => {
   return (
     <div>
-      <SongImage {...track.album}/>
-      <audio controls>
-        <source type="audio/mpeg" src={track.previewUrl}/>
-        Audio tag not supported
-      </audio>
-      <div>
-        <p>{track.name}</p>
-        <p>{track.artists.map(a => a.name).join(' ')}</p>
-      </div>
+      <p>{track.name}</p>
+      <p>{track.artists.map(a => a.name).join(' ')}</p>
     </div>
   );
 };
 
+export class CurrentTrack extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.audioPlayerRef = React.createRef();
+  }
+
+  // componentDidCatch(error, info) {
+  //   debugger;
+  //   console.log(`Info: ${info}`);
+  //   console.log(`Error: ${error}`);
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.audioPlayerRef.current == null) { return; }
+
+    this.audioPlayerRef.current.pause();
+    this.audioPlayerRef.current.load();
+    // this.audioPlayerRef.current.play();
+    this.audioPlayerRef.current.play().catch((e) => {
+      console.log(`Playback issue with: ${prevProps.track.name}`);
+      console.log(`Error: ${e}`);
+    });
+  }
+
+  render() {
+    let { track } = this.props;
+    if (track == null) { return null; }
+
+    return (
+      <div>
+        <SongImage {...track.album}/>
+        <audio controls ref={this.audioPlayerRef}>
+          <source type="audio/mpeg" src={track.previewUrl}/>
+          Audio tag not supported
+        </audio>
+        <TrackDetails track={track} />
+      </div>
+    );
+  }
+}
